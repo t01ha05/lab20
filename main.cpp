4d9e0d31139c438dd5af4b5622980414e5da8234
Talha Ahmed <t01haa2005@gmail.com>
2024-10-09 16:52:42 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-h
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Chair {
+    private:
+        int legs;
+        double prices[3];
+
+    public:
+        Chair() {
+            legs = (rand() % 2)+ 3;
+            for (int i=0; i < 3; ++i) {
+                const int MIN = 10000, MAX = 99999;
+                prices[i] = (rand() % (MAX - MIN +1) + MIN) / 100.0; 
+            }
+        }
+
+        Chair
+
+}
