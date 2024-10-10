93df0fd60aa3a98eb8212a88c374c96b55e24506
Talha Ahmed <t01haa2005@gmail.com>
2024-10-09 17:04:57 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -10,14 +10,41 @@ class Chair {
         double prices[3];
 
     public:
+        //default constructor
         Chair() {
-            legs = (rand() % 2)+ 3;
+            legs = (rand() % 2)+ 3; //randomly choose 3 or 4 legs
             for (int i=0; i < 3; ++i) {
                 const int MIN = 10000, MAX = 99999;
                 prices[i] = (rand() % (MAX - MIN +1) + MIN) / 100.0; 
             }
         }
+        //parameter constructor with legs and an array of prices
+        Chair(int nunLegs, const double priceArray[3]) {
+            leg = numLegs;
+            for (int i=0; i<3; ++i) {
+                prices [i] = priceArray[i];
+            }
+        }
+        //setter for legs
+        void setLegs(int numLegs){
+            legs = numLegs;
+        }
+        //setter for prices
+        void setPrices(const double priceArray[3]) {
+            for (int i = 0; i < 3; ++i) {
+                prices[i] = priceArray[i];
+            }
+        }
+        //print method to display chair details
+        void printDetails() const{
+            cout << "Chair with " << legs << " legs and prices: ";
+            for ( int i = 0; i < 3; ++i) {
+                cout << "$" << prices[i] << " ";
+            }
+            cout << endl;
+        }
+};
 
-        Chair
+int main() {
 
 }
